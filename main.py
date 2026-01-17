83e2ffa21b607e0ade44607bb19f28bac6f59ec7
Rishikesh Seelam <rishikeshseelam1248@gmail.com>
2026-01-17 10:50:04 -0800

    Add localhost to CORS

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://dermamole.netlify.app"],
+    allow_origins = ["https://dermamole.netlify.app", "http://localhost:5173"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
